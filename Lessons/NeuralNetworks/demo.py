f51e4cc6a0ed145685c4dea8c609a0013035e6ff
AsemRadhwi <asem.radhwi@gmail.com>
2017-02-09 23:18:24 +0300

    On submission

---
+++ Lessons/NeuralNetworks/demo.py
@@ -0,0 +1,12 @@
+from numpy import exp, array, random, dot
+
+class NeuralNetwork():
+    def __init__(self):
+        
+
+if __name__ == '__main__':
+
+    #initialise a single neuron neural network
+    nn = NeuralNetwork()
+
+    print('Random')
